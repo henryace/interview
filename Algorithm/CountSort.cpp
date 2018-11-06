44a5179c71b6ee7932d0c951f86e793c9290d3e5
20180104-135423\grtert <grtert@gmail.com>
2018-11-06 16:07:21 +0800

    2018/11/6

--- Algorithm/CountSort.cpp
+++ Algorithm/CountSort.cpp
@@ -1,52 +1,46 @@
-/*****************
-
-计数排序：统计小于等于该元素值的元素的个数i，于是该元素就放在目标数组的索引i位（i≥0）。
-
-计数排序基于一个假设，待排序数列的所有数均出现在（0，k）的区间之内，如果k过大则会引起较大的空间复杂度
-计数排序并非是一种基于比较的排序方法，它直接统计出键值本应该出现的位置
-时间复杂度为O（n），空间复杂度为O（n+k）
-
-*****************/
-
-
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-void countSort(vector<int>& vec,vector<int>& objVec)
-{
-	vector<int> range(10,0);    //range的下标即键值
-	for(int i=0;i<vec.size();++i)
-	{//统计每个键值出现的次数
-		range[vec[i]]++;
-	}
+// /*****************
+// 計數排序：統計小於等於該元素值的元素的個數i，於是該元素就放在目標數組的索引i位（i≥0）。
+// 計數排序基於一個假設，待排序數列的所有數均出現在（0，k）的區間之內，如果k過大則會引起較大的空間複雜度
+// 計數排序並非是一種基於比較的排序方法，它直接統計出鍵值本應該出現的位置
+// 時間複雜度為O（n），空間複雜度為O（n+k）
+// *****************/
 
-	for(int i=1;i<vec.size();++i)
-	{//后面的键值出现的位置为前面所有键值出现的次数之和
-		range[i]+=range[i-1];
-	}
-	//至此，range中存放的是相应键值应该出现的位置
-	int length=vec.size();
-	for(int i=length-1;i>=0;--i)        //注意一个小细节，统计时最正序的，这里是逆序
-	{//如果存在相同的键值，为了保持稳定性，后出现的应该还是位于后面
-		//如果正序，则先出现的会放置到后面，因此不再稳定
-		objVec[range[vec[i]]]=vec[i];   //将键值放到目标位置
-		range[vec[i]]--;
-	}
+void countSort(vector<int> &vec, vector<int> &objVec) {
+  vector<int> range(10, 0);              // range的下標即鍵值
+  for (int i = 0; i < vec.size(); ++i) { //統計每個鍵值出現的次數
+    range[vec[i]]++;
+  }
+
+  for (int i = 1; i < vec.size();
+       ++i) { //後面的鍵值出現的位置為前面所有鍵值出現的次數之和
+    range[i] += range[i - 1];
+  }
+  //至此，range中存放的是相應鍵值應該出現的位置
+  int length = vec.size();
+  for (int i = length - 1; i >= 0;
+       --i) //注意一個小細節，統計時最正序的，這裏是逆序
+  { //如果存在相同的鍵值，為了保持穩定性，後出現的應該還是位於後面
+    //如果正序，則先出現的會放置到後面，因此不再穩定
+    objVec[range[vec[i]]] = vec[i]; //將鍵值放到目標位置
+    range[vec[i]]--;
+  }
 }
 
-int main()
-{
-	int a[14]={0,5,7,9,6,3,4,5,2,8,6,9,2,1};
-	vector<int> vec(a,a+14);
-	vector<int> objVec(14,0);
+int main() {
+  int a[14] = {0, 5, 7, 9, 6, 3, 4, 5, 2, 8, 6, 9, 2, 1};
+  vector<int> vec(a, a + 14);
+  vector<int> objVec(14, 0);
 
-	countSort(vec,objVec);
+  countSort(vec, objVec);
 
-	for(int i=0;i<objVec.size();++i)
-		cout<<objVec[i]<<"  ";
-	cout<<endl;
-
-	return 0;
+  for (int i = 0; i < objVec.size(); ++i)
+    cout << objVec[i] << "  ";
+  cout << endl;
+  system("pause");
+  return 0;
 }
